daca8f60a953efae8ad8d176ba1f4c846085edce
oliver <be.like.chandu@gmail.com>
2024-01-11 19:57:59 +0530

    Cyclic Sort

---
+++ src/Shekhar/SearchingAndSorting/CycleSort.java
@@ -0,0 +1,26 @@
+package Shekhar.SearchingAndSorting;
+
+import java.util.Arrays;
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println("Array before sorting : " + Arrays.toString(arr));
+        cyclicSort(arr);
+        System.out.println("Array after sorting : " + Arrays.toString(arr));
+    }
+
+    private static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]){
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+}
