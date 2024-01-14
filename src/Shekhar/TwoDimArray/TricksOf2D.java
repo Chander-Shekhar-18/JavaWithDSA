d79a2d8bf035090d8f03b5333054dcb6174b88b6
oliver <be.like.chandu@gmail.com>
2024-01-14 16:23:53 +0530

    tricks of 2d array

---
+++ src/Shekhar/TwoDimArray/TricksOf2D.java
@@ -0,0 +1,32 @@
+package Shekhar.TwoDimArray;
+
+import java.util.Arrays;
+
+public class TricksOf2D {
+
+//    Print this using 2d array :
+//    0
+//    1 2
+//    3 4 5
+//    6 7 8 9
+    public static void main(String[] args) {
+        int[][] arr = new int[4][];
+        arr[0] = new int[1];
+        arr[1] = new int[2];
+        arr[2] = new int[3];
+        arr[3] = new int[2 + 2];
+
+        int k = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = k;
+                k++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
