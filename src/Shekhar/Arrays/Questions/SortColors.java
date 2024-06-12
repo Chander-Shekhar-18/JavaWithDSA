baeeb924356f1c8489520498eba918f0cb5af1d8
Chander-Shekhar-18 <be.like.chandu@gmail.com>
2024-06-12 18:55:47 +0530

    Sort color leetcode que

--- src/Shekhar/Arrays/Questions/SortColors.java
+++ src/Shekhar/Arrays/Questions/SortColors.java
@@ -1,4 +1,31 @@
 package Shekhar.Arrays.Questions;
 
+import java.util.Arrays;
+
 public class SortColors {
+    public static void main(String[] args) {
+        int[] arr = {2,0,2,1,1,0};
+        sortColors(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int idx = 0;
+
+        while (idx <= end && start < end) {
+            if (nums[idx] == 0){
+                nums[idx] = nums[start];
+                nums[start] = 0;
+                start++;
+                idx++;
+            } else if(nums[idx] == 2){
+                nums[idx] = nums[end];
+                nums[end] = 2;
+                end--;
+            }else {
+                idx++;
+            }
+        }
+    }
 }
